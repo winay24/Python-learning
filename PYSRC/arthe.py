0313634f36ca10afb17d0bc1ff63b281ef17a947
vinayreddy <kandukuri132@gmail.com>
2022-11-05 22:47:54 +0530

    started operators

---
+++ PYSRC/arthe.py
@@ -0,0 +1,33 @@
+#  // to get floor value
+print("printing // data")
+print(10/2)   # always float  5.0
+print(10.5/2) # always float 5.25
+print(10//2)  #print floor value of result that is befor .  => 5
+print(10.5//2)  #print floor value of 10.25  =>5.0 based on datatype
+
+# ** for power
+print("** operator data")
+print(10**2)
+print(10**3)
+
+# + operator
+print("testing + data")
+print(10 + 20)
+print("vinay"+"reddy") #concatenation
+#print("vinay"+24) #error both should be same datatype 
+print("vinay"+"24") #we can use str(24) also
+
+# * operator
+print("printing * data")
+print("vinay"*3) #3times print vinay it is same 3*vinay
+print("vinay"*3*3) # 3 times o/p of vinay*3
+#print("vinay"*"kumar") #error one operand must be int
+
+# / operator
+#print(10/0) # zero division error / // or % any operator or any value by 0
+
+#bool operator and *
+print("printing * with bool data")
+print("vinay" * True)
+print("vinay" * False)
+
---
+++ PYSRC/compar.py
@@ -0,0 +1,17 @@
+#comparison or relational operators  > ,< ,>= ,<=
+a = 10
+b = 20
+print("comparison of int")
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
+print("printing string comparison data")
+s1 = 'a'
+s2 = 'b'
+print(s1 < s2) #compars ASCII value of string 
+print(s1 <= s2)
+print(s1 > s2)
+print(s1 >= s2)
+print("ASCII VALUE OF a :",ord('a'))
+print("charcter with ASCII : ",chr(68))
